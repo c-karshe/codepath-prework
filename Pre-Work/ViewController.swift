5f78b8195938c031cc64b6a8b1bc17f666517aca
c-karshe <82768873+c-karshe@users.noreply.github.com>
2022-01-29 19:37:46 -0800

    added pre-work files to repository

---
+++ Pre-Work/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Pre-Work
+//
+//  Created by Muna Karshe on 1/28/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var TextLabel: UILabel!
+    
+    @IBAction func ButtonClicked(_ sender: Any) {
+        print("hello")
+        TextLabel.textColor = UIColor.brown
+    }
+    
+}
+
